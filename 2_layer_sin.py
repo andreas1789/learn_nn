7395cae33c86ab16be45158dcfc00016530eb646
andreas1789 <andreashoffmann1789@gmail.com>
2018-03-25 22:52:18 +0200

    damped sinus with omega fit

--- 2_layer_sin.py
+++ 2_layer_sin.py
@@ -2,14 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-
 num_nodes = 400
+
+plt.axis([0, 40, 0, 1])
+plt.ion()
 
 graph = tf.Graph()
 with graph.as_default():
-    x = tf.placeholder(tf.float32, shape=[None, 2])
+    x = tf.placeholder(tf.float32, shape=[None, 3])
     y = tf.placeholder(tf.float32, shape=[None, 1])
-    weights1 = tf.Variable(tf.truncated_normal(shape=[2, num_nodes]))
+    weights1 = tf.Variable(tf.truncated_normal(shape=[3, num_nodes]))
     weights2 = tf.Variable(tf.truncated_normal(shape=[num_nodes, num_nodes]))
     weights3 = tf.Variable(tf.truncated_normal(shape=[num_nodes, 1]))
     biases1 = tf.Variable(tf.zeros(num_nodes))
@@ -28,24 +30,26 @@ with tf.Session(graph=graph) as session:
     tf.global_variables_initializer().run()
     print("Initialized")
 
-    for i in range(100000):
-        tpoints = np.random.rand(100) * 20
+    for i in range(60000):
+        tpoints = np.random.rand(100) * 30
         omegapoints = np.random.rand(100) * 2
-        xpoints = np.array([tpoints, omegapoints]).T
+        taupoints = np.random.rand(100) * 1 
+        xpoints = np.array([tpoints, omegapoints, taupoints]).T
         
-        ypoints = np.sin(omegapoints*tpoints)/2+0.5
+        ypoints = np.exp(-taupoints * tpoints) * ( np.sin(omegapoints*tpoints) + 0.2*np.sin(6*omegapoints*tpoints) )/2+0.5
         
-        tpoints_eval = np.linspace(0,40,200)
+        tpoints_eval = np.linspace(0,40,400)
         omegapoints_eval = 0.7*np.ones(tpoints_eval.shape)
-        xpoints_eval = np.array([tpoints_eval, omegapoints_eval]).T
+        taupoints_eval = 0.1*np.ones(tpoints_eval.shape)
+        xpoints_eval = np.array([tpoints_eval, omegapoints_eval, taupoints_eval]).T
 
         _, loss_result = session.run([optimizer, loss], feed_dict={x: xpoints, y: ypoints[:, None]})
 
         if (i % 500 == 0):
             print('Step {}, loss={}'.format(i, loss_result))
-    
-    out = session.run(result, feed_dict={x: xpoints_eval})
-    plt.plot(tpoints_eval, out, tpoints_eval, np.sin(omegapoints_eval*tpoints_eval)/2+0.5)
-    plt.show()
+            plt.cla()
+            out = session.run(result, feed_dict={x: xpoints_eval})
+            plt.plot(tpoints_eval, out, tpoints_eval, np.exp(-taupoints_eval * tpoints_eval) * ( np.sin(omegapoints_eval*tpoints_eval) + 0.2*np.sin(6*omegapoints_eval*tpoints_eval) )/2+0.5)
+            plt.pause(0.05)
 
 
